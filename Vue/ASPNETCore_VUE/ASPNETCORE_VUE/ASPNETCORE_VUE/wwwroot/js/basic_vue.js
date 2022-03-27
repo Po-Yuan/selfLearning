030e227459d8227e1cf01156bbd7ae0ab6a263fc
Po-Yuan <l5778644@hotmail.com>
2022-03-27 14:25:21 +0800

    Vue
    test Vue

---
+++ Vue/ASPNETCore_VUE/ASPNETCORE_VUE/ASPNETCORE_VUE/wwwroot/js/basic_vue.js
@@ -0,0 +1,23 @@
+﻿//////Vue 3.0
+
+const { createApp, ref } = Vue;
+console.log(createApp);
+const vm = createApp({
+    setup() {
+        const message = ref('Test Vue 3.0');
+        return {
+            message
+        }
+    }
+});
+
+vm.mount('#app');
+
+//2.0的寫法 
+//const vm = new Vue({
+//    data: {
+//        message: 'hello vue!'
+//    }
+//});
+
+//vm.$mount('#app');
