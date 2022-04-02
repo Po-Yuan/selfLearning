f68f3723cc6f12b88fc83beddf9be78a980f351d
Po-Yuan <l5778644@hotmail.com>
2022-04-02 16:23:07 +0800

    basic vue
    basic vue

--- Vue/ASPNETCore_VUE/ASPNETCORE_VUE/ASPNETCORE_VUE/wwwroot/js/basic_vue.js
+++ Vue/ASPNETCore_VUE/ASPNETCORE_VUE/ASPNETCORE_VUE/wwwroot/js/basic_vue.js
@@ -5,13 +5,39 @@ console.log(createApp);
 const vm = createApp({
     setup() {
         const message = ref('Test Vue 3.0');
+        const count = ref(0);
         return {
-            message
+            message,
+            customId: "item_01",
+            isBtnDisabled: true,
+            isBtnHidden: false,
+            isSelected: true,
+            picked: 1,
+            checkedNames: ['Jack'],
+            count,
+            isShow: true,
+            notShow:false
+        }
+    },
+    computed: {
+        msgStyle: () => {
+            return {
+                'border': this.count === 6 ? "red solid 2px" : "",
+                'color': this.count === 6 ? "red" : ""
+            }
+        }
+    },
+    methods: {
+        plus(event) {
+            console.log(event);
+            console.log(event.target.tagName);
+            console.log(this.count);
+            this.count++;
         }
     }
-});
+}).mount('#app');
 
-vm.mount('#app');
+//vm.mount('#app');
 
 //2.0的寫法 
 //const vm = new Vue({
